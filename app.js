d8cb96f1cb372955a14d6cc260c2ab70495534f1
Rishabh5401 <rasus8800@gmail.com>
2025-09-26 20:13:45 +0530

    Fixed Internal Error of ejs template

--- app.js
+++ app.js
@@ -87,8 +87,10 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 app.use((req, res, next) => {
-    res.locals.currUser = req.session.user || null; // or req.user if using Passport
-    next();
+  res.locals.currUser = req.user; // or req.session.user if using sessions
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
 });
 
 
